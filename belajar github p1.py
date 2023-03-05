f054fcf15e182dd54c2b19884604c8ea2126083c
MUHAMMAD VITO ARISTAWIDYA <vitoarista861@gmail.com>
2023-03-06 01:03:28 +0700

    codingan 1

---
+++ belajar github p1.py
@@ -0,0 +1,7 @@
+def luas_segitiga() :
+    alas = int(input("masukkan alas segitiga : ")) 
+    tinggi = int(input("masukkan tinggi segitiga : "))
+    luas = alas*tinggi /2
+    print("Luas segitiga tersebut adalah =", luas)
+
+luas_segitiga()
